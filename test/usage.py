be5a0fe66ba83e5a1c9312a99542bc6682f35c10
fjt <deco@hotmail.com>
2024-11-27 16:28:49 +0800

    add hub operation

--- test/usage.py
+++ test/usage.py
@@ -1,28 +1,22 @@
-# dimo-sdk-python
-python sdk for dimo，include hub operations
-
-
-## install
-
-```shell
-pip install dimo-sdk
-```
-
-## usage
-
-```python
 from hub.client import Client
 
-base_url = "http://52.76.75.134:8080"  # Replace with the actual base URL
+base_url = "http://localhost:8080"  # Replace with the actual base URL
 client = Client(base_url)
 
 # Example parameters
 owner = "owner_name"
-filename = "example_meme.jpg"
+filename = "example_meme1.jpg"
 msg = "This is a test meme"
 
 # Upload meme message
 upload_response = client.upload_hub(owner, filename, msg)
+
+# Example for uploading actual data (e.g., image file)
+with open('test/client.py', 'rb') as f:
+    image_data = f.read()
+
+dataname = "data_meme1.jpg"
+upload_data_response = client.upload_hub_data(owner, dataname, image_data)
 
 # Example for downloading meme data
 downloaded_data = client.download_hub_data(owner,filename)
@@ -32,4 +26,3 @@ if downloaded_data:
     print("Download successful!")
 else:
     print("Download failed!")
-```
